61e3f0bb37bfacaa4f8857253f3fb0d1eb9c2868
swimmerRat <swim@jbnu.ac.kr>
2025-06-20 15:05:25 +0900

    헤더 만들기

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,12 +1,20 @@
 import style from './_style/page.module.css';
-export default function Home(){
+import './_style/global.css'
+
+export default function Home() {
   return (
-    <div className = {style.main}>
-      <h1>Hello World!</h1>
-      <div className={style.links}>
-        <span>Github</span>
-        <span>vlog</span>
-      </div>
+    <div className={style.main}>
+      <header className={style.header}>
+        <nav className={style.links}>
+          <a href="https://github.com/swimmerRat" target="_blank" rel="noopener noreferrer">
+            Github
+          </a>
+          <a href="https://suuuuwimmer.tistory.com/" target="_blank" rel="noopener noreferrer">
+            Blog
+          </a>
+        </nav>
+      </header>
+      {/* 나머지 페이지 내용 */}
     </div>
   )
 }
